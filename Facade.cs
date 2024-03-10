8381833f563d4b47928a2216c7e4f505d06d0dc8
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-10 13:25:54 +0200

    Facade

---
+++ Facade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class Facade
+    {
+        private SubsystemA _subsystemA;
+        private SubsystemB _subsystemB;
+        private SubsystemC _subsystemC;
+
+        public Facade()
+        {
+            _subsystemA = new SubsystemA();
+            _subsystemB = new SubsystemB();
+            _subsystemC = new SubsystemC();
+        }
+
+        public void Operation1()
+        {
+            _subsystemA.MethodA();
+            _subsystemB.MethodB();
+        }
+
+        public void Operation2()
+        {
+            _subsystemB.MethodB();
+            _subsystemC.MethodC();
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            var facade = new Facade();
+
+            facade.Operation1();
+            Console.WriteLine();
+            facade.Operation2();
+
+            Console.ReadKey();
+        }
+    }
+}
